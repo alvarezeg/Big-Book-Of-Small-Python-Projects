02c8c8df9e76c6e044328c15358d06c9e5d25ebc
Alvarez <104662445+alvarezeg@users.noreply.github.com>
2022-05-02 22:40:42 -0400

    Create bagels.py

---
+++ bagels.py
@@ -0,0 +1,10 @@
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-small-python-projects
+A version of this game is featured in the book "Invent Your Own Computer
+Games With Python" https://nostarch.com/inventwithypython
+Tags: short, gamem puzzle"""
+
+import random 
+
+NUM
